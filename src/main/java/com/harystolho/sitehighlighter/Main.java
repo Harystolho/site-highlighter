ab639e30a4b5686a073c462fe460158dc2dfa5fd
Harystolho <haristolhogamer@gmail.com>
2019-03-02 13:37:35 -0300

    Server runs on UTC timezone

---
+++ src/main/java/com/harystolho/sitehighlighter/Main.java
@@ -0,0 +1,16 @@
+package com.harystolho.sitehighlighter;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+
+	static {
+		System.setProperty("user.timezone", "UTC");
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
