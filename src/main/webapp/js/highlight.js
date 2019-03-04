b7e1384ae9f925df7b48b3596ee5764bac8dcf6b
Harystolho <haristolhogamer@gmail.com>
2019-03-04 14:11:43 -0300

    Show highlight modal when something is selected

--- src/main/webapp/js/highlight.js
+++ src/main/webapp/js/highlight.js
@@ -1,7 +1,50 @@
 (() => {
 
-    window.onmouseup = function () {
-        
+    let modalCss = `
+        width: 20px;
+        height: 20px;
+        background-colo: green;
+        position: absolute;
+    `;
+
+    let modalDiv = `<div id="highlightModal" style="${modalCss}">
+                        
+                    </div>`;
+
+    window.onload = () => {
+        loadModal();
+    };
+
+    window.onmouseup = () => {
+        openHighlightModal();
+    };
+
+    /**
+     * Creates the modal that shows up when something is selected
+     */
+    function loadModal() {
+        console.log("Inserting Highlight Modal");
+        document.body.innerHTML += modalDiv;
+    }
+
+    function openHighlightModal() {
+        let modal = document.querySelector("#highlightModal");
+
+        if (!isSomethingSelected()) {
+            modal.style.display = "none";
+            return;
+        }
+
+
+        modal.style.display = "block";
+    }
+
+    /**
+     *
+     * @return {boolean} TRUE if there is something(text or image) selected
+     */
+    function isSomethingSelected() {
+        return window.getSelection().anchorOffset !== window.getSelection().focusOffset;
     }
 
 })();
