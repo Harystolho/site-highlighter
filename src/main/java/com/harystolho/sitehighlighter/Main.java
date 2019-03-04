d149f9935956d13657f810c652f14693bc299871
Harystolho <haristolhogamer@gmail.com>
2019-03-04 13:51:52 -0300

    Add temporary website for testing

--- src/main/java/com/harystolho/sitehighlighter/Main.java
+++ src/main/java/com/harystolho/sitehighlighter/Main.java
@@ -1,9 +1,9 @@
 package com.harystolho.sitehighlighter;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
 	static {
@@ -11,6 +11,6 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(Main.class, args);
 	}
 }
