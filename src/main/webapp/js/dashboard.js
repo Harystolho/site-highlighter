e42339f94c8c41b6c6bdd00f703d9fe95c0cd10c
Harystolho <haristolhogamer@gmail.com>
2019-03-05 15:19:17 -0300

    Edit icon toggle between editing and not editing

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -2,29 +2,29 @@ let Dashboard = (() => {
     let funcs = {};
 
     window.onload = () => {
-      requestDocuments();
+        requestDocuments();
     };
 
     /**
      * A {Document} is a file that contains highlights in it.
      */
     function requestDocuments() {
-        httpGet("/api/v1/documents", (data)=>{
+        httpGet("/api/v1/documents", (data) => {
             let response = JSON.parse(data);
 
             let library = document.querySelector("#libraryContainer");
 
-            response.data.forEach((doc)=>{
+            response.data.forEach((doc) => {
                 library.innerHTML += funcs.librarySiteTemplate(doc.title, doc.path, doc.id);
             });
         });
     }
 
-    funcs.displayDocumentContent = (id) =>{
-        httpGet(`/api/v1/document/${id}`, (data)=>{
-           let response = JSON.parse(data);
+    funcs.displayDocumentContent = (id) => {
+        httpGet(`/api/v1/document/${id}`, (data) => {
+            let response = JSON.parse(data);
 
-           document.querySelector("#content").innerHTML = response.data.highlights;
+            document.querySelector("#content").innerHTML = response.data.highlights;
         });
     };
 
@@ -51,6 +51,24 @@ let Dashboard = (() => {
         xhttp.open("GET", url, true);
         xhttp.send();
     }
+
+    return funcs;
+})();
+
+let ContentEditor = (() => {
+    let funcs = {};
+
+    funcs.toggleEdit = () =>{
+        let content = document.querySelector("#content");
+
+        let state = content.getAttribute("contenteditable");
+
+        if(state === "true"){
+            content.setAttribute("contenteditable", "false");
+        } else {
+            content.setAttribute("contenteditable", "true");
+        }
+    };
 
     return funcs;
 })();
