ac8b00a481cf7068d1f848168517c791253a817b
Harystolho <haristolhogamer@gmail.com>
2019-03-05 07:56:40 -0300

    Add ServiceResponse as a way to communicate between the services and the controllers

---
+++ src/main/java/com/harystolho/sitehighlighter/service/HighlightService.java
@@ -0,0 +1,25 @@
+package com.harystolho.sitehighlighter.service;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.Cookie;
+
+import org.springframework.stereotype.Service;
+
+import com.harystolho.sitehighlighter.service.ServiceResponse.ServiceStatus;
+
+@Service
+public class HighlightService {
+
+	private static final Logger logger = Logger.getLogger(HighlightService.class.getName());
+
+	public ServiceResponse<Void> saveHighlight(Cookie[] cookies, String text) {
+		if (text == null) {
+			logger.severe("text is null");
+			return ServiceResponse.of(null, ServiceStatus.FAIL);
+		}
+		
+		return ServiceResponse.of(null, ServiceStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/harystolho/sitehighlighter/service/ServiceResponse.java
@@ -0,0 +1,37 @@
+package com.harystolho.sitehighlighter.service;
+
+public class ServiceResponse<T> {
+
+	public static enum ServiceStatus {
+		OK, FAIL
+	}
+
+	private T response;
+	private ServiceStatus status;
+
+	public ServiceResponse(T response, ServiceStatus status) {
+		this.response = response;
+		this.status = status;
+	}
+
+	public static <T> ServiceResponse<T> of(T response, ServiceStatus status) {
+		return new ServiceResponse<>(response, status);
+	}
+
+	public T getResponse() {
+		return response;
+	}
+
+	public void setResponse(T response) {
+		this.response = response;
+	}
+
+	public ServiceStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ServiceStatus status) {
+		this.status = status;
+	}
+
+}
