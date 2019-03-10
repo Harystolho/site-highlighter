692ee4152ecd55d9ad76dd2393f09ce72628f686
Harystolho <haristolhogamer@gmail.com>
2019-03-10 14:38:55 -0300

    Create Highlight Addon

---
+++ highlight-addon/script.js
@@ -0,0 +1,7 @@
+(()=>{
+	let script = document.createElement('script');
+	script.async = true;script.type = 'text/javascript';
+	script.src = "https://localhost:8181/js/highlight.js";
+	let node = document.getElementsByTagName('script')[0];
+	node.parentNode.insertBefore(script, node);
+})();
--- src/main/java/com/harystolho/sitehighlighter/Main.java
+++ src/main/java/com/harystolho/sitehighlighter/Main.java
@@ -19,6 +19,7 @@ public class Main {
 //TODO add option to choose where what document to add the highlight
 //TODO fix modal position
 
+//TODO add frame to see highlight in the page
 //TODO add display document on the page that is being highlighted
 
 //TODO add reminder
@@ -26,9 +27,9 @@ public class Main {
 //TODO add tag to document
 //TODO add sort by tag
 //TODO delete document
-//TODO add calendar to remainder's doc
 //TODO change document's name
+//TODO add calendar to remainder's doc
 
 //TODO add account/guest
 
-//TODO create addon
+// TODO remove the highlight page from the addon urls
