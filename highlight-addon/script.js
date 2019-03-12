a3d373188361bb86e3828392c698aed1cb8a463e
Harystolho <haristolhogamer@gmail.com>
2019-03-12 19:14:30 -0300

    Highlight addon fetches the script from dist/bundle.js

--- highlight-addon/script.js
+++ highlight-addon/script.js
@@ -2,7 +2,7 @@
 	let script = document.createElement('script');
 	script.async = true;
 	script.type = 'text/javascript';
-	script.src = "https://localhost:8181/js/highlight.js";
+	script.src = "https://localhost:8181/dist/bundle.js";
 	let node = document.getElementsByTagName('script')[0];
 	node.parentNode.insertBefore(script, node);
 })();
