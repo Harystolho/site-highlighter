c5f9fe8c1ded8aadf96a2d8a2e6edee781f43d30
Harystolho <haristolhogamer@gmail.com>
2019-03-12 16:06:22 -0300

    add css-loader and style-loader to import css files

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    watch: true
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 };
