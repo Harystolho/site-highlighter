38f60a341b6378143294a5a1bdab263a14c5b8f6
Harystolho <haristolhogamer@gmail.com>
2019-03-18 20:51:00 -0300

    Add CookieService

--- src/main/java/com/harystolho/sitehighlighter/controller/AccountController.java
+++ src/main/java/com/harystolho/sitehighlighter/controller/AccountController.java
@@ -1,23 +1,16 @@
 package com.harystolho.sitehighlighter.controller;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.harystolho.sitehighlighter.model.Document;
 import com.harystolho.sitehighlighter.service.AccountService;
-import com.harystolho.sitehighlighter.service.DocumentService;
 import com.harystolho.sitehighlighter.service.ServiceResponse;
 import com.harystolho.sitehighlighter.utils.API_Response;
 
--- src/main/java/com/harystolho/sitehighlighter/cookie/CookieService.java
+++ src/main/java/com/harystolho/sitehighlighter/cookie/CookieService.java
@@ -1,10 +1,13 @@
 package com.harystolho.sitehighlighter.cookie;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.http.Cookie;
 
 import org.springframework.stereotype.Service;
+
+import com.harystolho.sitehighlighter.model.Account;
 
 @Service
 public class CookieService {
@@ -12,12 +15,31 @@ public class CookieService {
 	private static final String HIGHLIGHT_ID = "highlight_id";
 	private static final int COOKIE_EXPIRATION = 60 * 60 * 24 * 7; // 7 DAYS
 
+	// Cookie Identifier, Account Id
+	private ConcurrentHashMap<String, String> cookies;
+
+	public CookieService() {
+		cookies = new ConcurrentHashMap<>();
+	}
+
+	/**
+	 * Binds the {@link Cookie}'s value to an {@link Account#getId()}
+	 * 
+	 * @param id {@link Account#id}
+	 * @return
+	 */
 	public Cookie createCookie(String id) {
 		Cookie cookie = new Cookie(HIGHLIGHT_ID, UUID.randomUUID().toString());
 		cookie.setPath("/");
 		cookie.setMaxAge(COOKIE_EXPIRATION);
 
+		cookies.put(cookie.getValue(), id);
+
 		return cookie;
+	}
+
+	public String getAccountId(Cookie cookie) {
+		return cookies.get(cookie.getValue());
 	}
 
 }
